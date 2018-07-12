UBERCAF/external_llvm

Merge changes from topic 'rs-rebase-275480'
* changes:
  Fix libLTO build rules for missing libraries.
  Fix *_EXTEND_VECTOR_INREG legalization
  Incremental squash-merge of aosp/dev to aosp/master
  Update aosp/master LLVM for rebase to r275480

lib/CodeGen/Android.mk
@@ -1,135 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 
-codegen_SRC_FILES := \
-  AggressiveAntiDepBreaker.cpp \
-  AllocationOrder.cpp \
-  Analysis.cpp \
-  AtomicExpandPass.cpp \
-  BasicTargetTransformInfo.cpp \
-  BranchFolding.cpp \
-  CalcSpillWeights.cpp \
-  CallingConvLower.cpp \
-  CodeGen.cpp \
-  CodeGenPrepare.cpp \
-  CoreCLRGC.cpp \
-  CriticalAntiDepBreaker.cpp \
-  DeadMachineInstructionElim.cpp \
-  DFAPacketizer.cpp \
-  DwarfEHPrepare.cpp \
-  EarlyIfConversion.cpp \
-  EdgeBundles.cpp \
-  ErlangGC.cpp \
-  ExecutionDepsFix.cpp \
-  ExpandISelPseudos.cpp \
-  ExpandPostRAPseudos.cpp \
-  FaultMaps.cpp \
-  FuncletLayout.cpp \
-  GCMetadata.cpp \
-  GCMetadataPrinter.cpp \
-  GCRootLowering.cpp \
-  GCStrategy.cpp \
-  GlobalMerge.cpp \
-  IfConversion.cpp \
-  ImplicitNullChecks.cpp \
-  InlineSpiller.cpp \
-  InterferenceCache.cpp \
-  InterleavedAccessPass.cpp \
-  IntrinsicLowering.cpp \
-  LatencyPriorityQueue.cpp \
-  LexicalScopes.cpp \
-  LiveDebugValues.cpp \
-  LiveDebugVariables.cpp \
-  LiveIntervalAnalysis.cpp \
-  LiveInterval.cpp \
-  LiveIntervalUnion.cpp \
-  LivePhysRegs.cpp \
-  LiveRangeCalc.cpp \
-  LiveRangeEdit.cpp \
-  LiveRegMatrix.cpp \
-  LiveStackAnalysis.cpp \
-  LiveVariables.cpp \
-  LLVMTargetMachine.cpp \
-  LocalStackSlotAllocation.cpp \
-  LowerEmuTLS.cpp \
-  MachineBasicBlock.cpp \
-  MachineBlockFrequencyInfo.cpp \
-  MachineBlockPlacement.cpp \
-  MachineBranchProbabilityInfo.cpp \
-  MachineCombiner.cpp \
-  MachineCopyPropagation.cpp \
-  MachineCSE.cpp \
-  MachineDominanceFrontier.cpp \
-  MachineDominators.cpp \
-  MachineFunctionAnalysis.cpp \
-  MachineFunction.cpp \
-  MachineFunctionPass.cpp \
-  MachineFunctionPrinterPass.cpp \
-  MachineInstrBundle.cpp \
-  MachineInstr.cpp \
-  MachineLICM.cpp \
-  MachineLoopInfo.cpp \
-  MachineModuleInfo.cpp \
-  MachineModuleInfoImpls.cpp \
-  MachinePassRegistry.cpp \
-  MachinePostDominators.cpp \
-  MachineRegionInfo.cpp \
-  MachineRegisterInfo.cpp \
-  MachineScheduler.cpp \
-  MachineSink.cpp \
-  MachineSSAUpdater.cpp \
-  MachineTraceMetrics.cpp \
-  MachineVerifier.cpp \
-  MIRPrinter.cpp \
-  MIRPrintingPass.cpp \
-  OcamlGC.cpp \
-  OptimizePHIs.cpp \
-  ParallelCG.cpp \
-  Passes.cpp \
-  PeepholeOptimizer.cpp \
-  PHIElimination.cpp \
-  PHIEliminationUtils.cpp \
-  PostRASchedulerList.cpp \
-  ProcessImplicitDefs.cpp \
-  PrologEpilogInserter.cpp \
-  PseudoSourceValue.cpp \
-  RegAllocBase.cpp \
-  RegAllocBasic.cpp \
-  RegAllocFast.cpp \
-  RegAllocGreedy.cpp \
-  RegAllocPBQP.cpp \
-  RegisterClassInfo.cpp \
-  RegisterCoalescer.cpp \
-  RegisterPressure.cpp \
-  RegisterScavenging.cpp \
-  ScheduleDAG.cpp \
-  ScheduleDAGInstrs.cpp \
-  ScheduleDAGPrinter.cpp \
-  ScoreboardHazardRecognizer.cpp \
-  ShrinkWrap.cpp \
-  ShadowStackGC.cpp \
-  ShadowStackGCLowering.cpp \
-  SjLjEHPrepare.cpp \
-  SlotIndexes.cpp \
-  SpillPlacement.cpp \
-  SplitKit.cpp \
-  StackColoring.cpp \
-  StackMapLivenessAnalysis.cpp \
-  StackMaps.cpp \
-  StackProtector.cpp \
-  StackSlotColoring.cpp \
-  StatepointExampleGC.cpp \
-  TailDuplication.cpp \
-  TargetFrameLoweringImpl.cpp \
-  TargetInstrInfo.cpp \
-  TargetLoweringBase.cpp \
-  TargetLoweringObjectFileImpl.cpp \
-  TargetOptionsImpl.cpp \
-  TargetRegisterInfo.cpp \
-  TargetSchedule.cpp \
-  TwoAddressInstructionPass.cpp \
-  UnreachableBlockElim.cpp \
-  VirtRegMap.cpp \
-  WinEHPrepare.cpp
+codegen_SRC_FILES := $(sort $(notdir $(wildcard $(LOCAL_PATH)/*.cpp)))
 
 # For the host
 # =====================================================

airend/android_external_clang

  Re-apply local patch dropped during 'git merge'
  Update aosp/master Clang for rebase to r275480

lib/CodeGen/Android.mk
@@ -16,54 +16,7 @@ clang_codegen_TBLGEN_TABLES := \
   StmtNodes.inc \
   arm_neon.inc
 
-clang_codegen_SRC_FILES := \
-  BackendUtil.cpp \
-  CGAtomic.cpp \
-  CGBlocks.cpp \
-  CGBuiltin.cpp \
-  CGCall.cpp \
-  CGClass.cpp \
-  CGCleanup.cpp \
-  CGCUDANV.cpp \
-  CGCUDARuntime.cpp \
-  CGCXXABI.cpp \
-  CGCXX.cpp \
-  CGDebugInfo.cpp \
-  CGDecl.cpp \
-  CGDeclCXX.cpp \
-  CGException.cpp \
-  CGExprAgg.cpp \
-  CGExprComplex.cpp \
-  CGExprConstant.cpp \
-  CGExpr.cpp \
-  CGExprCXX.cpp \
-  CGExprScalar.cpp \
-  CGLoopInfo.cpp \
-  CGObjC.cpp \
-  CGObjCGNU.cpp \
-  CGObjCMac.cpp \
-  CGObjCRuntime.cpp \
-  CGOpenCLRuntime.cpp \
-  CGOpenMPRuntime.cpp \
-  CGRecordLayoutBuilder.cpp \
-  CGStmt.cpp \
-  CGStmtOpenMP.cpp \
-  CGVTables.cpp \
-  CGVTT.cpp \
-  CodeGenABITypes.cpp \
-  CodeGenAction.cpp \
-  CodeGenFunction.cpp \
-  CodeGenModule.cpp \
-  CodeGenPGO.cpp \
-  CodeGenTBAA.cpp \
-  CodeGenTypes.cpp \
-  CoverageMappingGen.cpp \
-  ItaniumCXXABI.cpp \
-  MicrosoftCXXABI.cpp \
-  ModuleBuilder.cpp \
-  ObjectFilePCHContainerOperations.cpp \
-  SanitizerMetadata.cpp \
-  TargetInfo.cpp
+clang_codegen_SRC_FILES := $(sort $(notdir $(wildcard $(LOCAL_PATH)/*.cpp)))
 
 # For the host
 # =====================================================